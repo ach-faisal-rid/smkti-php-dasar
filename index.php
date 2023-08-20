aef01a1f6f062f1900b6cd189327a10263b2ac85
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-20 11:38:49 +0700

    perubahan dan penambahan kode

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// echo "hello world"; menyimpan hello
 $nama = "faisal";
-echo "nama saya ".$nama;
+$salam = "assalamualaikum";
+
+$hasil = $salam. " nama saya " .$nama; // variabel
+echo $hasil; // menampilkan data yang disimpan dari variabel
+//echo "nama saya faisal"; memanggil dan menampilkan
 ?>
