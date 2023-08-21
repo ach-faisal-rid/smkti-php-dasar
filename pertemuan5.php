d0cb484901335990574402e3acb62ce627fa7ce0
ach-faisal-rid <faizal.a.ridhani@gmail.com>
2023-08-21 19:00:17 +0700

    perubahan nama dari index menjadi pertemuan

--- pertemuan5.php
+++ pertemuan5.php
@@ -1,9 +1,13 @@
 <?php
 // echo "hello world"; menyimpan hello
 $nama = "faisal";
+$umur = "20";
+$hobi = ['memancing', 'bersepeda', 'berenang']; // array yang menyimpan data
 $salam = "assalamualaikum";
 
-$hasil = $salam. " nama saya " .$nama; // variabel
+$hasil = "$salam. nama saya .$nama.  umur saya  . $umur"; // variabel
 echo $hasil; // menampilkan data yang disimpan dari variabel
+
+print_r($hobi); // debugging
 //echo "nama saya faisal"; memanggil dan menampilkan
 ?>
